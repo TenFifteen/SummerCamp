ca98fd51410a0d9bbb23d8ee4c55879ffb7dc9bc
libenchao <libenchao@gmail.com>
2015-07-26 15:47:50 +0800

    add unique-paths-ii

---
+++ lbc/63-unique-paths-ii.cc
@@ -0,0 +1,37 @@
+/*
+题目大意：
+与62题一样，但是矩阵中有障碍物。
+
+解题思路：
+动态规划。如果当前是障碍物，那么当前点的路是0；
+
+遇到的问题：
+无。
+*/
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        vector<int> tmp(obstacleGrid[0].size(), 0);
+        vector<vector<int>> ans(obstacleGrid.size(), tmp);
+        if(obstacleGrid[0][0] == 1)return 0;
+        ans[0][0] = 1;
+        for(int i = 1; i < obstacleGrid[0].size(); ++i){
+            if(ans[0][i-1] == 1 && obstacleGrid[0][i] == 0){
+                ans[0][i] = 1;
+            }
+        }
+        for(int i = 1; i < obstacleGrid.size(); ++i){
+            if(ans[i-1][0] == 1 && obstacleGrid[i][0] == 0){
+                ans[i][0] = 1;
+            }
+            for(int j = 1; j < obstacleGrid[0].size(); ++j){
+                if(obstacleGrid[i][j] == 1){
+                    ans[i][j] = 0;
+                }else{
+                    ans[i][j] = ans[i-1][j] + ans[i][j-1];
+                }
+            }
+        }
+        return ans[obstacleGrid.size()-1][obstacleGrid[0].size()-1];
+    }
+};
