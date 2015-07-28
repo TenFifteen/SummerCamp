459b41441bcabc681c71e5e01c1a45d6c73c16fc
songpengwei <songpengwei40@gmail.com>
2015-07-28 19:27:05 +0800

    most voted code, use preorder

---
+++ spw/102/most_voted_code.cc
@@ -0,0 +1,17 @@
+vector<vector<int>> ret;
+
+void buildVector(TreeNode *root, int depth)
+{
+	if(root == NULL) return;
+	if(ret.size() == depth)
+		ret.push_back(vector<int>());
+
+	ret[depth].push_back(root->val);
+	buildVector(root->left, depth + 1);
+	buildVector(root->right, depth + 1);
+}
+
+vector<vector<int> > levelOrder(TreeNode *root) {
+	buildVector(root, 0);
+	return ret;
+}
