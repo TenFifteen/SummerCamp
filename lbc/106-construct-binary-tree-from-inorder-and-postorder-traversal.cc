3c422801ce270c84e7703e91535d1d671a0c7179
libenchao <libenchao@gmail.com>
2015-07-28 10:16:54 +0800

    add construct-binary-tree-from-inorder-and-postorder-traversal

---
+++ lbc/106-construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -0,0 +1,35 @@
+/*
+题目大意：
+给定一个二叉树的中序遍历和后序遍历，重构二叉树。
+
+解题思路：
+递归。
+
+遇到的问题：
+一次通过。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return sub(inorder, 0, inorder.size()-1, postorder, 0, postorder.size()-1);
+    }
+    
+    TreeNode *sub(vector<int> &in, int in_left, int in_right, vector<int> &post, int post_left, int post_right){
+        if(in_left > in_right)return NULL;
+        TreeNode *cur = new TreeNode(post[post_right]);
+        int index = in_left;
+        while(in[index] != post[post_right])index++;
+        cur->left = sub(in, in_left, index-1, post, post_left, post_left+index-in_left-1);
+        cur->right = sub(in, index+1, in_right, post, post_left+index-in_left, post_right-1);
+        return cur;
+    }
+};
