98a1d639e42210ed139f85ab5428dfa11dc5e2ec
libenchao <libenchao@gmail.com>
2015-07-29 09:16:25 +0800

    add minimum-depth-of-binary-tree

---
+++ lbc/111-minimum-depth-of-binary-tree.cc
@@ -0,0 +1,28 @@
+/*
+题目大意：
+给定一个二叉树，求出其根到叶子节点中的最短的路径。
+
+解题思路：
+递归
+
+遇到的问题：
+一开始没注意到叶子节点是left和right同时都是NULL的情况。。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL)return NULL;
+        if(root->left == NULL)return minDepth(root->right)+1;
+        if(root->right == NULL)return minDepth(root->left)+1;
+        return min(minDepth(root->left), minDepth(root->right))+1;
+    }
+};
