da2669271963ee87dcedd60e92362d5cd80e900e
libenchao <libenchao@gmail.com>
2015-07-29 09:56:31 +0800

    add distinct-subsequences

---
+++ lbc/115-distinct-subsequences.cc
@@ -0,0 +1,33 @@
+/*
+题目大意：
+给定一个字符串s，给以目标串t。在s的所有子序列中，有多少个t。
+
+解题思路：
+动态规划
+
+遇到的问题：
+竟然把s和t搞反了。
+*/
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        if(s.size() < t.size())return 0;
+        vector<vector<int>> dp(t.size()+1, vector<int>(s.size()+1, 0));
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == t[0]){
+                dp[1][i+1] = dp[1][i]+1;
+            }else{
+                dp[1][i+1] = dp[1][i];
+            }
+        }
+        for(int i = 1; i < t.size(); ++i){
+            for(int j = i; j < s.size(); ++j){
+                dp[i+1][j+1] = dp[i+1][j];
+                if(t[i] == s[j]){
+                    dp[i+1][j+1] += dp[i][j];
+                }
+            }
+        }
+        return dp[t.size()][s.size()];
+    }
+};
