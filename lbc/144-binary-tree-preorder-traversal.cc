93e99fd8e8838f0d619bbd869c1340d92b899c4b
libenchao <libenchao@gmail.com>
2015-08-05 10:07:32 +0800

    add binary-tree-preorder-traversal

---
+++ lbc/144-binary-tree-preorder-traversal.cc
@@ -0,0 +1,36 @@
+/*
+题目大意：
+先序遍历二叉树，不用递归。
+
+解题思路：
+用一个stack将需要访问的节点缓存住。
+
+遇到的问题：
+一次通过。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL)return ans;
+        stack<TreeNode *> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode *cur = s.top();
+            s.pop();
+            ans.push_back(cur->val);
+            if(cur->right != NULL)s.push(cur->right);
+            if(cur->left != NULL)s.push(cur->left);
+        }
+        return ans;
+    }
+};
