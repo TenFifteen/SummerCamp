f729a1dd461bd51fde6d1f5946b00d107af2d8d4
songpengwei <songpengwei40@gmail.com>
2015-08-16 07:06:11 +0800

    binary right side view

---
+++ spw/199/binary_right_side_view.cc
@@ -0,0 +1,42 @@
+void bfs(vector<int> &ans, queue<TreeNode *> &q) {
+	if (q.empty()) return;
+
+	queue<TreeNode *> next;
+	ans.push_back(q.back()->val); 
+	while (!q.empty()) {
+		TreeNode *now = q.front();
+		q.pop();
+		if (now->left) next.push(now->left);
+		if (now->right) next.push(now->right);
+	}
+
+	bfs(ans, next);
+}
+
+void dfs(vector<int> &ans, TreeNode* root, int depth) {
+	if (!root) return;
+
+	if (depth == ans.size()) ans.push_back(root->val);
+	dfs(ans, root->right, depth+1);
+	dfs(ans, root->left, depth+1);
+}
+
+/**
+ * Problem: given a binary tree, return a vector contains the value from top to the bottom
+ *          when you see from the right side of the tree.
+ * Solve: bfs or dfs, when dfs, use the sequence: root->right->left. We can also think sth
+ *        about see from the left view, see from the bottom (root->right->left and now->left
+ *        == NULL && now->right == NULL).
+ * Tips: no
+ */
+vector<int> rightSideView(TreeNode* root) {
+	vector<int> ans;
+	/*if (root == NULL) return ans;
+	 *         
+	 *queue<TreeNode *> q;
+	 *q.push(root);
+	 *bfs(ans, q);*/
+
+	dfs(ans, root, 0);
+	return ans;
+}
