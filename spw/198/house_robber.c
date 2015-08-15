3c1c7bc69178846ee5d7e5c100b0c1a312ddf570
songpengwei <songpengwei40@gmail.com>
2015-08-16 06:39:34 +0800

    house robber

---
+++ spw/198/house_robber.c
@@ -0,0 +1,22 @@
+/**
+ * Problem: given an array, find the max sum of the non-adjacent
+ *          element(which is non-negative).
+ * Solve: something like knapsack, because the elements can't be negative
+ *        we just keep two variable, the maximum sum end of the previous
+ *        previous element and end of the previous element.
+ * Tips: no
+ */
+#define max(a, b) ((a) > (b) ? (a) : (b))
+int rob(int* nums, int numsSize) {
+	int i;
+	int pre1 = 0, pre2 = 0;
+	for (i = 0; i < numsSize; ++i) {
+		if (i % 2 == 0) {
+			pre1 = max(pre1+nums[i], pre2);
+		} else {
+			pre2 = max(pre2+nums[i], pre1);
+		}
+	}
+
+	return max(pre1, pre2);
+}
