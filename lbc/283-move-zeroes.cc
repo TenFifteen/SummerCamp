764eafb0a240e6384aef073a844b9c37a6c833e9
libenchao <libenchao@gmail.com>
2015-12-17 16:29:53 +0800

    add 283-move-zeroes

---
+++ lbc/283-move-zeroes.cc
@@ -0,0 +1,24 @@
+/*
+题目大意：
+给定一个数组，将其中的0都移到最后去，但是要保持其他数字的相对位置。
+
+解题思路：
+见代码
+
+遇到的问题：
+没有问题。
+*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        while (index < nums.size()) {
+            nums[index++] = 0;
+        }
+    }
+};
