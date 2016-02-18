4868fff03fd2e67a71130a90a5bf442bed562742
李本超 <libenchao@gmail.com>
2016-02-18 22:22:26 +0800

    rewrite some code of leetcode

--- lbc/018-4sum.cc
+++ lbc/018-4sum.cc
@@ -64,3 +64,46 @@ public:
         return ans;
     }
 };
+/*
+第二次做：
+这次做的已经很熟练了，直接成功了。
+*/
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if (nums.size() < 4) return ans;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size()-3; ++i) {
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            
+            for (int j = i+1; j < nums.size()-2; ++j) {
+                if (j != i+1 && nums[j] == nums[j-1]) continue;
+                
+                int left = j+1, right = nums.size()-1;
+                while (left < right) {
+                    int cur = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (cur == target) {
+                        vector<int> tmp(4);
+                        tmp[0] = nums[i];
+                        tmp[1] = nums[j];
+                        tmp[2] = nums[left];
+                        tmp[3] = nums[right];
+                        ans.push_back(tmp);
+                        
+                        while (left+1 < right && nums[left+1] == nums[left]) left++;
+                        left++;
+                    } else if (cur < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
