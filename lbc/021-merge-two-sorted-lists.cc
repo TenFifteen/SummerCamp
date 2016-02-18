f53d4599671d0efab2e57f3bac2c5f06e3b699eb
李本超 <libenchao@gmail.com>
2016-02-18 22:42:47 +0800

    rewrite some code of leetcode

--- lbc/021-merge-two-sorted-lists.cc
+++ lbc/021-merge-two-sorted-lists.cc
@@ -87,3 +87,42 @@ public:
         return ret;
     }
 };
+/*
+第二次做：
+这次做的很顺畅。
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode prehead(0);
+        ListNode *head = &prehead;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            } else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        
+        if (l1) {
+            head->next = l1;
+        }
+        if (l2) {
+            head->next = l2;
+        }
+        
+        return prehead.next;
+    }
+};
