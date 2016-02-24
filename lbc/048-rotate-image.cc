3a66d7ca66a0abb686624b8b0d9ef078580ba5ad
李本超 <libenchao@gmail.com>
2016-02-24 21:04:40 +0800

    re-write some code of leetcode

--- lbc/048-rotate-image.cc
+++ lbc/048-rotate-image.cc
@@ -28,3 +28,23 @@ public:
         }
     }
 };
+/*
+第二次做：
+跟上次一样的解法。虽然那种按照层次结构来做的也挺好的，不过我已经先入为主了。
+*/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        for (int i = 0; i < (n+1)/2; ++i) {
+            for (int j = 0; j < n/2; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+};
