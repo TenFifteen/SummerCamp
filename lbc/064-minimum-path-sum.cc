5b32287e00cbee5a978725828cf4d40d0a64c0d2
李本超 <libenchao@gmail.com>
2016-02-25 21:03:52 +0800

    re-write some code of leetcode

--- lbc/064-minimum-path-sum.cc
+++ lbc/064-minimum-path-sum.cc
@@ -31,3 +31,24 @@ public:
         return ans[grid[0].size()-1];
     }
 };
+/*
+第二次做：
+比较简单的动归。不过一开始初始化条件搞错了。
+*/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.size() == 0 || grid[0].size() == 0) return 0;
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<int> ans(n, INT_MAX);
+        ans[0] = 0;
+        for (int i = 0; i < m; ++i) {
+            ans[0] += grid[i][0];
+            for (int j = 1; j < n; ++j) {
+                ans[j] = min(ans[j-1], ans[j]) + grid[i][j];
+            }
+        }
+        return ans[n-1];
+    }
+};
