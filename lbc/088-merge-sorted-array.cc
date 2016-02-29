e34b8952b7cb2a011671f6d87228abea50cdfa82
李本超 <libenchao@gmail.com>
2016-02-29 15:54:50 +0800

    re-write some code of leetcode

--- lbc/088-merge-sorted-array.cc
+++ lbc/088-merge-sorted-array.cc
@@ -56,3 +56,21 @@ public:
         }
     }
 };
+/*
+第二次做：
+简单题，一次通过。
+*/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i1 = m-1, i2 = n-1, i = m+n-1;
+        while (i1 >= 0 && i2 >= 0) {
+            if (nums1[i1] > nums2[i2]) {
+                nums1[i--] = nums1[i1--];
+            } else {
+                nums1[i--] = nums2[i2--];
+            }
+        }
+        while (i2 >= 0) nums1[i--] = nums2[i2--];
+    }
+};
