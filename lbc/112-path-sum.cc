0612b4608980e1b1447eb5efd164a4595ae2163b
李本超 <libenchao@gmail.com>
2016-03-01 10:00:14 +0800

    re-write some code of leetcode

--- lbc/112-path-sum.cc
+++ lbc/112-path-sum.cc
@@ -42,3 +42,26 @@ public:
         return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
     }
 };
+/*
+第二次做：
+简单题。不过这种题要注意，一定是叶子节点。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL) return false;
+        if (root->left == NULL && root->right == NULL) return root->val == sum;
+        if (root->left == NULL) return hasPathSum(root->right, sum-root->val);
+        if (root->right == NULL) return hasPathSum(root->left, sum-root->val);
+        return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+    }
+};
