f862e14a3087eb51d22af26cd04b1ec81b7ca7ad
李本超 <libenchao@gmail.com>
2016-03-01 09:36:42 +0800

    re-write some code of leetcode

--- lbc/108-convert-sorted-array-to-binary-search-tree.cc
+++ lbc/108-convert-sorted-array-to-binary-search-tree.cc
@@ -36,3 +36,35 @@ public:
         return cur;
     }
 };
+/*
+第二次做：
+简单题。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode *build(vector<int> &nums, int left, int right) {
+        if (left > right) return NULL;
+        
+        int mid = left + ((right-left+1) >> 1);
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = build(nums, left, mid-1);
+        root->right = build(nums, mid+1, right);
+        
+        return root;
+    }
+
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) return NULL;
+        return build(nums, 0, nums.size()-1);
+    }
+};
