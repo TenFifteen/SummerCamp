043327013d3c023ccfacacc6baeb1d53a7bf9189
李本超 <libenchao@gmail.com>
2016-03-02 08:55:23 +0800

    re-write some code of leetcode

--- lbc/121-best-time-to-buy-and-sell-stock.cc
+++ lbc/121-best-time-to-buy-and-sell-stock.cc
@@ -30,3 +30,21 @@ public:
         else return ret;
     }
 };
+/*
+第二次做：
+买股票系列的最简单的题目。
+*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+        
+        int buy = prices[0], ans = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            ans = max(ans, prices[i]-buy);
+            buy = min(buy, prices[i]);
+        }
+        
+        return ans;
+    }
+};
