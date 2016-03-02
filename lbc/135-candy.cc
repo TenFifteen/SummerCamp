0a5f25fd31bc8937c146febfb7d99fc0a8454034
李本超 <libenchao@gmail.com>
2016-03-02 15:55:33 +0800

    re-write some code of leetcode

--- lbc/135-candy.cc
+++ lbc/135-candy.cc
@@ -38,3 +38,36 @@ public:
         return ret;
     }
 };
+/*
+第二次做：
+这次想明白为什么这样子做是对的了。
+试想一下，如果ratings[k] > ratings[k-1]， 那么在反过来遍历的时候，
+ratings[k]只会增长，不会降低；而ratings[k-1]肯定是不会变的。所以
+两边的条件都满足了。
+不过在写的时候，还是忘记了一个条件： && ans[i] <= ans[i+1]
+*/
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.size() == 0) return 0;
+        
+        int n = ratings.size();
+        vector<int> ans(n, 1);
+        
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i-1]) {
+                ans[i] = ans[i-1]+1;
+            }
+        }
+        
+        for (int i = n-2; i >= 0; --i) {
+            if (ratings[i] > ratings[i+1] && ans[i] <= ans[i+1]) {
+                ans[i] = ans[i+1] + 1;
+            }
+        }
+        
+        int sum = 0;
+        for (auto a : ans) sum += a;
+        return sum;
+    }
+};
