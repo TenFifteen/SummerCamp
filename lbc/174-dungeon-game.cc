59a9d775c522340842797868f8c536390359d9f3
李本超 <libenchao@gmail.com>
2016-03-03 16:58:12 +0800

    re-write some code of leetcode

--- lbc/174-dungeon-game.cc
+++ lbc/174-dungeon-game.cc
@@ -80,3 +80,45 @@ public:
         return dp[dp.size()-1][dp[0].size()-1] > 0;
     }
 };
+/*
+第二次做：
+这次做的还不错，思路清晰，代码简洁。只是中间有一点点小问题。
+*/
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size();
+        assert(m > 0);
+        int n = dungeon[0].size();
+        assert(n > 0);
+        
+        int left = 1, right = INT_MAX;
+        while (left < right) {
+            int mid = left + ((right-left)>>1);
+            
+            vector<int> dp(n);
+            dp[0] = dungeon[0][0] + mid;
+            for (int i = 1; i < n; ++i) {
+                if (dp[i-1] <= 0) dp[i] = 0;
+                else dp[i] = dp[i-1] + dungeon[0][i];
+            }
+            
+            for (int i = 1; i < m; ++i) {
+                if (dp[0] > 0) dp[0] += dungeon[i][0];
+                for (int j = 1; j < n; ++j) {
+                    int health = 0;
+                    if (dp[j-1] > 0) health = max(dp[j-1]+dungeon[i][j], health);
+                    if (dp[j] > 0) health = max(dp[j]+dungeon[i][j], health);
+                    dp[j] = health;
+                }
+            }
+            
+            if (dp[n-1] > 0) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+};
