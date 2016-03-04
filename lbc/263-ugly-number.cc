b29ec060b7a5efe00bdeaeea888c8c4dcadccfe9
李本超 <libenchao@gmail.com>
2016-03-04 21:09:19 +0800

    re-write some code of leetcode

--- lbc/263-ugly-number.cc
+++ lbc/263-ugly-number.cc
@@ -25,3 +25,18 @@ public:
         return num == 1;
     }
 };
+/*
+第二次做：
+简单题。
+*/
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num < 1) return false;
+        
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+};
