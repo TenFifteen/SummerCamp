4d3a72526fc6e48f1eb6cb8d229f4804693859df
李本超 <libenchao@gmail.com>
2016-03-04 20:12:24 +0800

    re-write some code of leetcode

--- lbc/238-product-of-array-except-self.cc
+++ lbc/238-product-of-array-except-self.cc
@@ -51,3 +51,25 @@ public:
         return ret;
     }
 };
+/*
+第二次做：
+这次还好，自己找到了空间O(1)的算法了。
+*/
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(), 1);
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        
+        int right = 1;
+        for (int i = nums.size()-2; i >= 0; --i) {
+            right *= nums[i+1];
+            ans[i] *= right;
+        }
+        
+        return ans;
+    }
+};
