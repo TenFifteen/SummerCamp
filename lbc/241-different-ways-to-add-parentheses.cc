5a6792c6d0f987cf207261aeadf6bdf65b6171e2
李本超 <libenchao@gmail.com>
2016-03-04 20:53:03 +0800

    re-write some code of leetcode

--- lbc/241-different-ways-to-add-parentheses.cc
+++ lbc/241-different-ways-to-add-parentheses.cc
@@ -173,3 +173,31 @@ public:
         }
     }
 };
+/*
+第二次做：
+这次的代码比之前简洁多了。
+*/
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> ans;
+        if (input.size() == 0) return ans;
+        
+        for (int i = 0; i < input.size(); ++i) {
+            if (input[i] == '-' || input[i] == '+' || input[i] == '*') {
+                auto left = diffWaysToCompute(input.substr(0, i));
+                auto right = diffWaysToCompute(input.substr(i+1));
+                for (auto l : left) {
+                    for (auto r : right) {
+                        if (input[i] == '-') ans.push_back(l-r);
+                        else if (input[i] == '+') ans.push_back(l+r);
+                        else ans.push_back(l*r);
+                    }
+                }
+            }
+        }
+        
+        if (ans.size() == 0) ans.push_back(stoi(input));
+        return ans;
+    }
+};
