5b68de0171980a7a0ceb58b122f1adb83f538b0a
李本超 <libenchao@gmail.com>
2016-03-05 11:43:50 +0800

    re-write some code of leetcode

--- lbc/284-peeking-iterator.cc
+++ lbc/284-peeking-iterator.cc
@@ -78,3 +78,57 @@ public:
 	    return ci != eci;
 	}
 };
+/*
+第二次做：
+这次写的还可以。
+*/
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+    bool buf;
+    int last;
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    buf = false;
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        if (!buf) {
+            buf = true;
+            last = Iterator::next();
+        }
+        return last;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    if (buf) {
+	        buf = false;
+	        return last;
+	    }
+	    return Iterator::next();
+	}
+
+	bool hasNext() const {
+	    return buf || Iterator::hasNext();
+	}
+};
