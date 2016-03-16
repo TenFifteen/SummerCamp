04236798b476ed84cd9f9f974bf36dfdd36a4806
root <songpengwei40@gmail.com>
2016-03-16 21:17:16 +0800

    nim game

---
+++ spw/192/292/nim_game.cc
@@ -0,0 +1,14 @@
+/**
+ * Problem:
+ * Solve: no
+ * Tips: no
+ */
+class Solution {
+    public:
+        bool canWinNim(int n) {
+            if (n % 4 == 0) return false;
+            return true;
+
+        }
+
+};
