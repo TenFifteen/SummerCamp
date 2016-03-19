d6d6c46b418558dbc0907d0cda69f20b04cce519
root <songpengwei40@gmail.com>
2016-03-19 21:33:49 +0800

    wiggle sort ii

--- spw_lintcode/507-wiggle-sort-ii/solve.cc
+++ spw_lintcode/507-wiggle-sort-ii/solve.cc
@@ -2,6 +2,18 @@
  * Problem: Given an unsorted array nums, reorder it such that
  *           nums[0] < nums[1] > nums[2] < nums[3]....
  * Solve: find the middle number and use the three partition.
+ *        Why we use three partition, because in the position map
+ *        we use, only the middle elements will have chance to stay
+ *        together if we use two partition.
+ *        index map:
+ *        virtual index:
+ *        index:     0  1  2  3   4   5  6  7  8  9
+ *        number:   18 17 19 16  15  11 14 10 13 12
+ *
+ *        real index:
+ *        index:     5  0  6  1  7  2  8  3  9  4
+ *        number:   11 18 14 17 10 19 13 16 12 15
+ *        the element around 15 have been place in two polar.
  * Tips: see comments below.
  */
 class Solution {
