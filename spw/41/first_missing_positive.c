922f74a48c5ec8e0d1c8734a90cc7dcc0f252b30
root <songpengwei40@gmail.com>
2016-04-05 20:12:05 +0800

    mod some wrong words

--- spw/41/first_missing_positive.c
+++ spw/41/first_missing_positive.c
@@ -11,19 +11,19 @@ void swap(int v[], int a, int b)
 	v[a] = v[a] ^ v[b];
 }
 /**
- * Problem: I think the decription is puzzling. It should be: given an array of N, 
+ * Problem: I think the decription is puzzling. It should be: given an array of N,
  *          find the missing integer in the sequences 1...N, because the given array
- *          may contains dupblicates or the number out of range.
- * Solve: place each number in its palce. For example, put 3 to a[2], put 1 to a[0].
- * Tips: see the comment below 
+ *          may contains duplicates or the number out of range.
+ * Solve: place each number in its place. For example, put 3 to a[2], put 1 to a[0].
+ * Tips: see the comment below
  */
-int firstMissingPositive(int* nums, int numsSize) 
+int firstMissingPositive(int* nums, int numsSize)
 {
 	int i;
 	for (i = 0; i < numsSize; ++i) {
 		// important!! we shuould swap until we get right number(i+1) in the place i
 		// Only in this way, we'll not miss some swap by which we place the number we just put before in right place.
-		// For example, when i = 2, and nums[i] = 4, nums[3] = 1, we swap(nums, 2, 3), but the 1 we'll never have 
+		// For example, when i = 2, and nums[i] = 4, nums[3] = 1, we swap(nums, 2, 3), but the 1 we'll never have
 		// opportunity to put it in right place.
 		while (nums[i] > 0 && nums[i] <= numsSize && nums[i] != nums[nums[i]-1]) {
 			swap(nums, i, nums[i]-1);
