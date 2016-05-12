616ab0c8a239b8149d7230ddf90545786af57b51
root <songpengwei40@gmail.com>
2016-05-12 11:35:44 +0800

    a+b problem;

---
+++ spw_lintcode/001-a-plus-b/solve.cc
@@ -0,0 +1,17 @@
+/**
+ * Problem: Write a function that add two numbers A and B.
+ *          You should not use + or any arithmetic operators.
+ * Solve: just add
+ * Tips: no
+ */
+class Solution {
+    public:
+        /*
+         * @param a: The first integer
+         * @param b: The second integer
+         * @return: The sum of a and b
+         */
+        int aplusb(int a, int b) {
+            return a + b;
+        }
+};
