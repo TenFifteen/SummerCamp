25e64547b8c3b7f6b42118d5250cad2bff3ee2d6
root <songpengwei40@gmail.com>
2016-05-16 15:19:27 +0800

    a+b

---
+++ spw_hihocoder/1000-a-plus-b/solve.cc
@@ -0,0 +1,18 @@
+/**
+ * Problem: a + b
+ * Solve: when a and b are too large.
+ * Tips: no
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while (cin >> a >> b) {
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
