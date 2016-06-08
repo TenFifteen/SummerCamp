79a162b3b9fa34efd5290232080e2332b02fa903
songpengwei <songpengwei40@gmail.com>
2016-06-08 15:29:40 +0800

    ds

---
+++ spw_hackerrank/array/DS.cc
@@ -0,0 +1,47 @@
+/**
+ * Problem: reversely print a array
+ * Solve: just do it
+ * Tips: no
+ */
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+        cin >> arr[arr_i];
+    }
+
+    for (int i = n-1; i >= 0; --i) {
+        cout << arr[i];
+        if (i == 0) cout << endl;
+        else cout << " ";
+    }
+    return 0;
+}
+
