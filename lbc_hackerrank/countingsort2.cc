bb2ad6c44f8a44fb3292ce93026b08e1a301bd9b
Benchao Li <libenchao@gmail.com>
2016-06-09 13:20:05 +0800

    Add countingsort2

---
+++ lbc_hackerrank/countingsort2.cc
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    vector<int> count(100, 0);
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cin >> num;
+        count[num]++;
+    }
+    for (int i = 0; i < count.size(); ++i) {
+        for (int j = 0; j < count[i]; ++j) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
