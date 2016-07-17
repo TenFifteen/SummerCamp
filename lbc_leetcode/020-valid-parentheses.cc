6975501aa37149cf76220ec13f5eb0b3d83e2715
libenchao <libenchao@gmail.com>
2016-07-17 13:16:06 +0800

    Add 020-valid-parentheses

--- lbc_leetcode/020-valid-parentheses.cc
+++ lbc_leetcode/020-valid-parentheses.cc
@@ -64,7 +64,30 @@ public:
                 stk.pop();
             }
         }
-        
+
         return stk.empty();
+    }
+};
+/*
+ * 尚可
+ */
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') stk.push(s[i]);
+            else if (s[i] == ')') {
+                if (stk.size() == 0 || stk.top() != '(') return false;
+                stk.pop();
+            } else if (s[i] == ']') {
+                if (stk.size() == 0 || stk.top() != '[') return false;
+                stk.pop();
+            } else if (s[i] == '}') {
+                if (stk.size() == 0 || stk.top() != '{') return false;
+                stk.pop();
+            }
+        }
+        return stk.size() == 0;
     }
 };
