274ac06bc8dd81788514fe5b32ec47291fc59df2
libenchao <libenchao@gmail.com>
2016-07-23 14:27:15 +0800

    Add 064-minimum-path-sum

--- lbc_leetcode/064-minimum-path-sum.cc
+++ lbc_leetcode/064-minimum-path-sum.cc
@@ -40,7 +40,7 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         if (grid.size() == 0 || grid[0].size() == 0) return 0;
         int m = grid.size(), n = grid[0].size();
-        
+
         vector<int> ans(n, INT_MAX);
         ans[0] = 0;
         for (int i = 0; i < m; ++i) {
@@ -50,5 +50,23 @@ public:
             }
         }
         return ans[n-1];
+    }
+};
+/*
+ * some easy
+ */
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < m; ++i) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; ++j) {
+                dp[j] = min(dp[j], dp[j-1]) + grid[i][j];
+            }
+        }
+        return dp[n-1];
     }
 };
