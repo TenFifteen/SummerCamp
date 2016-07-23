f2f8f81bd05f394153cda07582041c402c3e9380
libenchao <libenchao@gmail.com>
2016-07-23 18:07:32 +0800

    Add 074-search-a-2d-matrix

--- lbc_leetcode/074-search-a-2d-matrix.cc
+++ lbc_leetcode/074-search-a-2d-matrix.cc
@@ -39,7 +39,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if (matrix.size() == 0 || matrix[0].size() == 0) return false;
-        
+
         int m = matrix.size(), n = matrix[0].size();
         int left = 0, right = m*n-1;
         while (left <= right) {
@@ -48,6 +48,29 @@ public:
             if (matrix[x][y] == target) return true;
             else if (matrix[x][y] > target) right = mid-1;
             else left = mid+1;
+        }
+        return false;
+    }
+};
+/*
+ * some easy
+ */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+        if (n == 0) return false;
+
+        int left = 0, right = n*m-1;
+        while (left <= right) {
+            int mid = (left+right)/2;
+            int x = mid/n;
+            int y = mid%n;
+            if (matrix[x][y] == target) return true;
+            if (matrix[x][y] < target) left = mid+1;
+            else right = mid-1;
         }
         return false;
     }
