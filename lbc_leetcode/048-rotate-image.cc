0b9567be4b0eaed3790ba5d8dbec81fdc5af3a86
libenchao <libenchao@gmail.com>
2016-07-23 09:37:07 +0800

    Add 048-rotate-image

--- lbc_leetcode/048-rotate-image.cc
+++ lbc_leetcode/048-rotate-image.cc
@@ -36,7 +36,25 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        
+
+        for (int i = 0; i < (n+1)/2; ++i) {
+            for (int j = 0; j < n/2; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+};
+/*
+ *还行
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
         for (int i = 0; i < (n+1)/2; ++i) {
             for (int j = 0; j < n/2; ++j) {
                 int tmp = matrix[i][j];
