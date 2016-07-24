6218c7d2530e70cc5240bcf36a080ec9579d1365
libenchao <libenchao@gmail.com>
2016-07-24 16:03:57 +0800

    Add 100-same-tree

--- lbc_leetcode/100-same-tree.cc
+++ lbc_leetcode/100-same-tree.cc
@@ -61,3 +61,23 @@ public:
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
+/*
+ * too easy
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) return p == q;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
