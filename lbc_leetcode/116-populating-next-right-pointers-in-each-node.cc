479abff10df4f6c987fc60bb41835a9f80268e93
libenchao <libenchao@gmail.com>
2016-07-25 22:41:37 +0800

    Add 116-populating-next-right-ppointers-in-each-node

--- lbc_leetcode/116-populating-next-right-pointers-in-each-node.cc
+++ lbc_leetcode/116-populating-next-right-pointers-in-each-node.cc
@@ -41,7 +41,7 @@ public:
     void connect(TreeLinkNode *root) {
         sub(root, NULL, NULL);
     }
-    
+
     void sub(TreeLinkNode *root, TreeLinkNode *fa, TreeLinkNode *neigbor){
         if(root == NULL)return;
         if(fa == NULL){
@@ -77,13 +77,44 @@ class Solution {
 public:
     void connect(TreeLinkNode *root) {
         if (root == NULL || root->left == NULL) return;
-        
+
         root->left->next = root->right;
         if (root->next) {
             root->right->next = root->next->left;
         }
-        
+
         connect(root->left);
         connect(root->right);
+    }
+};
+/*
+ * some easy
+ */
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    void sub(TreeLinkNode *root, TreeLinkNode *fa) {
+        if (root == NULL) return;
+        if (fa != NULL) {
+            if (root == fa->left) {
+                root->next = fa->right;
+            } else if (fa->next != NULL) {
+                root->next = fa->next->left;
+            }
+        }
+        sub(root->left, root);
+        sub(root->right, root);
+    }
+
+public:
+    void connect(TreeLinkNode *root) {
+        sub(root, NULL);
     }
 };
