f2ef51095cf53f872e81043b2d8412f254951d95
libenchao <libenchao@gmail.com>
2016-07-26 00:00:32 +0800

    Add 121-best-time-to-buy-and-sell-stock

--- lbc_leetcode/121-best-time-to-buy-and-sell-stock.cc
+++ lbc_leetcode/121-best-time-to-buy-and-sell-stock.cc
@@ -38,13 +38,28 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() < 2) return 0;
-        
+
         int buy = prices[0], ans = 0;
         for (int i = 1; i < prices.size(); ++i) {
             ans = max(ans, prices[i]-buy);
             buy = min(buy, prices[i]);
         }
-        
+
+        return ans;
+    }
+};
+/*
+ * some easy
+ */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+        int buy = prices[0], ans = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            ans = max(ans, prices[i] - buy);
+            buy = min(buy, prices[i]);
+        }
         return ans;
     }
 };
