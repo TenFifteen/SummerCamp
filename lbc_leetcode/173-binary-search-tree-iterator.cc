5441ca42df2c94ede5b348ff48b03c22cd08a790
libenchao <libenchao@gmail.com>
2016-07-30 22:44:27 +0800

    Add 173-binary-search-tree-iterator'

--- lbc_leetcode/173-binary-search-tree-iterator.cc
+++ lbc_leetcode/173-binary-search-tree-iterator.cc
@@ -92,7 +92,54 @@ public:
     int next() {
         auto cur = s.top(); s.pop();
         int ret = cur->val;
-        
+
+        cur = cur->right;
+        while (cur) {
+            s.push(cur);
+            cur = cur->left;
+        }
+        return ret;
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
+/*
+ * ok
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+private:
+    stack<TreeNode *> s;
+
+public:
+    BSTIterator(TreeNode *root) {
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return s.size() != 0;
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *cur = s.top(); s.pop();
+        int ret = cur->val;
         cur = cur->right;
         while (cur) {
             s.push(cur);
