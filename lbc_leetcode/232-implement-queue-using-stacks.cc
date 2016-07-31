8595b36c83ae432ae00e5fe35faa237c529d89b9
libenchao <libenchao@gmail.com>
2016-07-31 20:38:02 +0800

    Add 232-implement-queue-using-stacks

--- lbc_leetcode/232-implement-queue-using-stacks.cc
+++ lbc_leetcode/232-implement-queue-using-stacks.cc
@@ -88,3 +88,43 @@ public:
         return s1.size() == 0 && s2.size() == 0;
     }
 };
+/*
+ * good
+ */
+class Queue {
+private:
+    stack<int> in, out;
+
+public:
+    // Push element x to the back of queue.
+    void push(int x) {
+        in.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        if (out.size() == 0) {
+            while (in.size() > 0) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        out.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        if (out.size() == 0) {
+            while (in.size() > 0) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return in.size() == 0 && out.size() == 0;
+    }
+};
