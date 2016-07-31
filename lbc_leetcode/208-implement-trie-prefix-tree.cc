c2dbe6b23e7ba86e4b9ffecd15084a028eeab4d3
libenchao <libenchao@gmail.com>
2016-07-31 15:24:02 +0800

    Add 208-implement-trie-prefix-tree

--- lbc_leetcode/208-implement-trie-prefix-tree.cc
+++ lbc_leetcode/208-implement-trie-prefix-tree.cc
@@ -133,3 +133,61 @@ private:
 // Trie trie;
 // trie.insert("somestring");
 // trie.search("key");
+/*
+ * good
+ */
+class TrieNode {
+public:
+    // Initialize your data structure here.
+    bool isWord;
+    unordered_map<char, TrieNode *> next;
+    TrieNode() {
+        isWord = false;
+    }
+};
+
+class Trie {
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    void insert(string word) {
+        TrieNode *node = root;
+        for (char ch : word) {
+            if (node->next.find(ch) == node->next.end()) node->next[ch] = new TrieNode();
+            node = node->next[ch];
+        }
+        node->isWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string word) {
+        TrieNode *node = root;
+        for (char ch : word) {
+            if (node->next.find(ch) == node->next.end()) return false;
+            node = node->next[ch];
+        }
+        return node->isWord;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    bool startsWith(string prefix) {
+        TrieNode *node = root;
+        for (char ch : prefix) {
+            if (node->next.find(ch) == node->next.end()) return false;
+            node = node->next[ch];
+        }
+        return true;
+    }
+
+private:
+    TrieNode* root;
+};
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie;
+// trie.insert("somestring");
+// trie.search("key");
