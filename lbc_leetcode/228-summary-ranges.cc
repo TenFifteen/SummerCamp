6789b96b70de69cc9e8014b8ac1e58f325e42db2
libenchao <libenchao@gmail.com>
2016-07-31 21:14:56 +0800

    Add 228-summary-ranges

--- lbc_leetcode/228-summary-ranges.cc
+++ lbc_leetcode/228-summary-ranges.cc
@@ -37,19 +37,40 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
         if (nums.size() == 0) return ans;
-        
+
         int last = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] == nums[i-1]+1) continue;
-            
+
             if (nums[i-1] != last) ans.push_back(to_string(last) + "->" + to_string(nums[i-1]));
             else ans.push_back(to_string(last));
             last = nums[i];
         }
-        
+
         if (nums.back() == last) ans.push_back(to_string(last));
         else (ans.push_back(to_string(last) + "->" + to_string(nums.back())));
-        
+
+        return ans;
+    }
+};
+/*
+ * ok
+ */
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        if (nums.size() == 0) return ans;
+
+        int index = 0;
+        while (index < nums.size()) {
+            int next = index+1;
+            while (next < nums.size() && nums[next] == nums[next-1]+1) next++;
+            if (next-1 != index) ans.push_back(to_string(nums[index]) + "->" + to_string(nums[next-1]));
+            else ans.push_back(to_string(nums[index]));
+
+            index = next;
+        }
         return ans;
     }
 };
