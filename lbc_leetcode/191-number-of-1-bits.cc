8f3807ac88defae2378d90d737dfc6b8d91c367f
libenchao <libenchao@gmail.com>
2016-07-31 10:33:20 +0800

    Add 191-number-of-1-bits

--- lbc_leetcode/191-number-of-1-bits.cc
+++ lbc_leetcode/191-number-of-1-bits.cc
@@ -13,11 +13,11 @@
 但是ICS里还有一种更好的：
 int hammingWeight(uint32_t n)
 {
-    n = (n & 0x55555555) + (n >>  1 & 0x55555555); // put count of each  2 bits into those  2 bits 
-    n = (n & 0x33333333) + (n >>  2 & 0x33333333); // put count of each  4 bits into those  4 bits 
-    n = (n & 0x0F0F0F0F) + (n >>  4 & 0x0F0F0F0F); // put count of each  8 bits into those  8 bits 
-    n = (n & 0x00FF00FF) + (n >>  8 & 0x00FF00FF); // put count of each 16 bits into those 16 bits 
-    n = (n & 0x0000FFFF) + (n >> 16 & 0x0000FFFF); // put count of each 32 bits into those 32 bits 
+    n = (n & 0x55555555) + (n >>  1 & 0x55555555); // put count of each  2 bits into those  2 bits
+    n = (n & 0x33333333) + (n >>  2 & 0x33333333); // put count of each  4 bits into those  4 bits
+    n = (n & 0x0F0F0F0F) + (n >>  4 & 0x0F0F0F0F); // put count of each  8 bits into those  8 bits
+    n = (n & 0x00FF00FF) + (n >>  8 & 0x00FF00FF); // put count of each 16 bits into those 16 bits
+    n = (n & 0x0000FFFF) + (n >> 16 & 0x0000FFFF); // put count of each 32 bits into those 32 bits
     return n;
 }
 */
@@ -36,6 +36,20 @@ public:
 第二次做：
 ICS方法做的，很好。
 */
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+        return n;
+    }
+};
+/*
+ * it's good
+ */
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
