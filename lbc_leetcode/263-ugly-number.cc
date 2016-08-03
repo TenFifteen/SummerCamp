9c6258d09b5e66e190bc924f6ce01c8eb9931129
libenchao <libenchao@gmail.com>
2016-08-03 22:53:15 +0800

    Add 263-ugly-number

--- lbc_leetcode/263-ugly-number.cc
+++ lbc_leetcode/263-ugly-number.cc
@@ -17,11 +17,11 @@ class Solution {
 public:
     bool isUgly(int num) {
         if (num <= 0) return false;
-        
+
         while (num % 2 == 0) num /= 2;
         while (num % 3 == 0) num /= 3;
         while (num % 5 == 0) num /= 5;
-        
+
         return num == 1;
     }
 };
@@ -33,7 +33,20 @@ class Solution {
 public:
     bool isUgly(int num) {
         if (num < 1) return false;
-        
+
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+};
+/*
+ * too easy
+ */
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num < 1) return false;
         while (num % 2 == 0) num /= 2;
         while (num % 3 == 0) num /= 3;
         while (num % 5 == 0) num /= 5;
