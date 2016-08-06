b926de15c2f63d39b0f440178f8244ef4675203d
libenchao <libenchao@gmail.com>
2016-08-06 10:40:29 +0800

    Add 304-range-sum-query-2d-immutable

--- lbc_leetcode/304-range-sum-query-2d-immutable.cc
+++ lbc_leetcode/304-range-sum-query-2d-immutable.cc
@@ -24,7 +24,7 @@ public:
             }
             buf.push_back(tmp);
         }
-        
+
         if (matrix.size() != 0) for (int i = 0; i < matrix[0].size(); ++i) {
             for (int j = 1; j < matrix.size(); ++j) {
                 buf[j][i] += buf[j-1][i];
@@ -36,7 +36,7 @@ public:
         if (row1 == 0 && col1 == 0) return buf[row2][col2];
         if (row1 == 0) return buf[row2][col2] - buf[row2][col1-1];
         if (col1 == 0) return buf[row2][col2] - buf[row1-1][col2];
-        return buf[row2][col2] + buf[row1-1][col1-1] - buf[row1-1][col2] - buf[row2][col1-1]; 
+        return buf[row2][col2] + buf[row1-1][col1-1] - buf[row1-1][col2] - buf[row2][col1-1];
     }
 };
 
@@ -58,13 +58,13 @@ public:
         if (matrix.size() == 0 || matrix[0].size() == 0) return;
         int m = matrix.size(), n = matrix[0].size();
         sum = vector<vector<long long>>(m+1, vector<long long>(n+1, 0));
-        
+
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 sum[i][j] = sum[i-1][j] + matrix[i-1][j-1];
             }
         }
-        
+
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 sum[i][j] += sum[i][j-1];
@@ -82,3 +82,35 @@ public:
 // NumMatrix numMatrix(matrix);
 // numMatrix.sumRegion(0, 1, 2, 3);
 // numMatrix.sumRegion(1, 2, 3, 4);
+/*
+ * ok
+ */
+class NumMatrix {
+private:
+    vector<vector<long long>> sum;
+
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) return; // corner case...
+        sum = vector<vector<long long>>(matrix.size()+1, vector<long long>(matrix[0].size()+1));
+        for (int i = 0; i < matrix.size(); ++i) {
+            long long line = 0;
+            for (int j = 0; j < matrix[0].size(); ++j) {
+                line += matrix[i][j];
+                sum[i+1][j+1] = sum[i][j+1] + line;
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
+    }
+};
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
+
+
