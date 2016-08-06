2142d4529e500070cd4a7fef314780a37dc87560
libenchao <libenchao@gmail.com>
2016-08-06 23:09:15 +0800

    Add 338-counting-bits

--- lbc_leetcode/338-counting-bits.cc
+++ lbc_leetcode/338-counting-bits.cc
@@ -12,11 +12,24 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> ans(num+1, 0);
-        
+
         for (int i = 1; i <= num; ++i) {
             ans[i] = ans[i&(i-1)] + 1;
         }
-        
+
+        return ans;
+    }
+};
+/*
+ * resolve it again. good.
+ */
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> ans(num+1);
+        for (int i = 1; i <= num; ++i) {
+            ans[i] = ans[i&(i-1)] + 1;
+        }
         return ans;
     }
 };
